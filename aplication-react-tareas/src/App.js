771b63d03cef614a75e34eb6fce8d1c1b1900f70
cristian <cbarrerar@unal.edu.co>
2024-11-22 11:15:58 -0500

    Component Task Completed

--- aplication-react-tareas/src/App.js
+++ aplication-react-tareas/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import logoApp from './images/codeblack_logo.png';
+import Task from './components/Task';
+
 
 function App() {
   return (
     <div className="App">
-        Melo
+      <div className='logoAplicacion-container'>
+        <img
+          className='logoApp-logo'
+          src={logoApp}
+          alt='logo de la app'/>
+      </div>  
+      <div className='principalList'>
+        <h1> Mis Tareas</h1>
+        <Task text='Learn React'/>
+      </div>
     </div>
   );
 }
---
+++ aplication-react-tareas/src/components/Task.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../stylesheets/Task.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+
+function Task({ text, completed }){
+    return(
+        <div className={completed ? "container-task completed" : "container-task"}>
+            <div className="text-task">
+                {text}
+            </div>
+            <div className="container-icon-task">
+                <AiOutlineCloseCircle className="icon-task"/>
+            </div>
+        </div>
+    );
+}
+
+export default Task;
